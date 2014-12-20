7dab3405c1526322a570ed15cc671d422ad73ede
Quark Wei <quark.j.wei@gmail.com>
2014-12-20 13:41:39 +0900

    Adding actions/url-patterns complete. Working on removing action/url-patterns.

--- src/chrome/src/options/js/data-handling.js
+++ src/chrome/src/options/js/data-handling.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("body").on("click", ".remove-item", function(){
-    $(this).parent().parent().remove();
+    $(this).parentsUntil("form").remove();
   });
   $(".add-item").click(function(){
     var panel = $("#template").children(":nth-child(1)").clone();
@@ -9,36 +9,54 @@ $(document).ready(function() {
     $("form").append(panel);
   });
   $("body").on("click", ".add-url-pattern", function(){
-    //If need to add new formGroup
     addNewFormGroup.call(this, "url-pattern");
-    //If need to make url-pattern span visible
   });
   $("body").on("click", ".add-action", function(){
-    //If need to add new formGroup
     addNewFormGroup.call(this, "action");
-    //If need to make action span visible
   });
   $("body").on("click", ".remove-url-pattern", function(){
     //If need to remove formGroup
-
+    removeFormGroup.call(this, "url-pattern");
     //If need to make url-pattern span invisible
   });
   $("body").on("click", ".remove-action", function(){
     //If need to remove formGroup
-
+    removeFormGroup.call(this, "action");
     //If need to make action span invisible
   });
 });
 function addNewFormGroup(type){
+  var invisibleUrlPatterns = $(".remove-url-pattern").parentsUntil(".form-group", ".invisible");
+  var invisibleActions = $(".remove-action").parentsUntil(".form-group", ".invisible");
+  if(type == "url-pattern" && invisibleUrlPatterns.length){
+    invisibleUrlPatterns.last().removeClass("invisible");
+    return;
+  } else if(type == "action" && invisibleActions.length){
+    invisibleActions.last().removeClass("invisible");
+    return;
+  }
   var formGroup = $("#template").children(":nth-child(2)").clone();
   formGroup.find(".display-once").addClass("invisible");
   formGroup.find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
   formGroup.find(".add-action").removeClass("add-action").addClass("remove-action");
   formGroup.find(".add-url-pattern").removeClass("add-url-pattern").addClass("remove-url-pattern");
+
   if(type == "url-pattern"){
-    formGroup.find(".remove-action").parent().parent().addClass("invisible");
+    formGroup.find(".remove-action").parentsUntil(".form-group", ".input-group").addClass("invisible");
   } else if(type == "action") {
-    formGroup.find(".remove-url-pattern").parent().parent().addClass("invisible");
+    formGroup.find(".remove-url-pattern").parentsUntil(".form-group", ".input-group").addClass("invisible");
   }
+
   $(this).parent().parent().parent().parent().append(formGroup);
+}
+function removeFormGroup(type){
+  var invisibleUrlPatterns = $(".remove-url-pattern").parentsUntil(".form-group", ".invisible");
+  var invisibleActions = $(".remove-action").parentsUntil(".form-group", ".invisible");
+  if(type == "url-pattern" && invisibleUrlPatterns.length){
+    $(this).parentsUntil(".form-group", ".input-group").addClass("invisible");
+    return;
+  } else if(type == "action" && invisibleActions.length){
+    $(this).parentsUntil(".form-group", ".input-group").addClass("invisible");
+    return;
+  }
 }
