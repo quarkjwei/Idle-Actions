5b4a0b76e54dcbfbc7c5bd8bac56e7de5b8a3ff7
John Heng <hengj@vcu.edu>
2015-03-05 11:19:22 -0500

    Final modifications, cleaned up code

--- src/firefox/page-mod/data/my-script.js
+++ src/firefox/page-mod/data/my-script.js
@@ -1,42 +1,14 @@
-function inIframe(){
-    if(top != self){
-         var contentHeight = $('#myIframeContent').height(); //Just this part I did with jQuery as I was sure that the document uses it
-         postSize(contentHeight);
-         }
-    }
-
-function postSize(height){
-     var target = parent.postMessage ? parent : (parent.document.postMessage ? parent.document : undefined);
-
-    if(typeof target != "undefined" && document.body.scrollHeight){
-        target.postMessage(height, "*");
-        }
-    }    
-
-function receiveSize(e){
-    if(e.origin === "http://www.mywebsite.net"){
-        var newHeight = e.data + 35;
-        document.getElementById("myIframeID").style.height = newHeight + "px";
-        }
-    }
-
-window.addEventListener("message", receiveSize, false);    
-
 var inactivityTime = function () {
     var t;
     idleTime = 30000;
     idleUnit = 1000;
 
     self.port.on("getIdleTime", function(time){
-        //console.log("getIdleTime: " + time);
         idleTime = time;
-        //console.log("idleTime: " + idleTime);
     })
 
     self.port.on("getIdleUnit", function(unit){
-        //console.log("getIdleUnit: " + unit);
         idleTime = idleTime * (unit * 1000); //convert seconds to milliseconds
-        //console.log("idleUnit: " + unit);
         document.onload = setTimer(idleTime);
     })
 
@@ -46,34 +18,19 @@ var inactivityTime = function () {
     window.onscroll = resetTimer;    // catches scrolling with arrow keys
     window.onkeypress = resetTimer;
 
-    /*window.onblur = ;
-    window.onfocus = ;*/
-
     function logout(modifier) {
         console.log("page is now idle");
         self.port.emit("script-response", "logout");
     }
 
     function setTimer(){
-        console.log("setTime: " + idleTime);
-        //var x = false;
         t = setTimeout(logout, idleTime);
-
-        console.log(new Date().toTimeString() + ": timer set to " + idleTime);
     }
 
     function resetTimer() {
-        clearTimeout(t); 
-        console.log("resetTimer: " + idleTime);
-        
-        
+        clearTimeout(t);
         t = setTimeout(logout, idleTime);
-        console.log(new Date().toTimeString() + ": active");
     }
-
-    /*function onBlur() {
-        if()
-    }*/
 };
 
 inactivityTime();
--- src/firefox/page-mod/lib/main.js
+++ src/firefox/page-mod/lib/main.js
@@ -1,27 +1,3 @@
-require("sdk/ui/button/action").ActionButton({
-  id: "list-tabs",
-  label: "List Tabs",
-  icon: "./icon-16.png",
-  onClick: listTabs
-});
-
-//onClick
-function listTabs() {
-  var tabs = require("sdk/tabs");
-  tabs.open("http://twitter.com");
-  for (let tab of tabs)
-  {
-    runScript(tab);
-    console.log(tab.url);
-  }
-}
- 
-function runScript(tab) {
-  tab.attach({
-    contentScript: "document.body.style.border = '5px solid red';"
-  });
-}
-
 // Import the page-mod API
 var pageMod = require("sdk/page-mod");
 var self = require("sdk/self");
@@ -30,49 +6,13 @@ var notifications = require("sdk/notifications");
 
 //use this to pull preferences from the simple-prefs sdk
 var preferences = require("sdk/simple-prefs").prefs;
- 
-// Create a page mod
-// It will run a script whenever a pattern is matched with the below array
-// The script goes to a URL after an idling time
-/*pageMod.PageMod({
-  include: "*",
-  contentScriptFile: self.data.url("my-script.js"),
-
-  onAttach: function(worker) {
-    var time = preferences.idleTime;
-    var unit = preferences.idleUnit;
-    var list = preferences.urlList;
-
-    var urlList = list.split(",");
-
-    worker.port.emit("getIdleTime", time);
-    worker.port.emit("getIdleUnit", unit);
-    
-    worker.port.on("script-response", function(response){
-      for(var x = 0; x < urlList.length; x++)
-      {
-        tabs.open({
-          url: urlList[x],
-          inBackground: true,
-          onLoad: function close(tab){
-            tab.close();
-          }
-        });
-      }  
-      notifications.notify({
-        text: "You were logged out",
-      });
-    });
-
-    worker.port.on("tabBlur", function(response){
-      worker.destroy();
-    })
-  }
-});*/
 
 var time = preferences.idleTime;
 var unit = preferences.idleUnit;
 var list = preferences.urlList;
+var notificationPref = preferences.notification;
+
+require("sdk/simple-prefs").on("", changePreference);
 
 var urlList = list.split(",");
 
@@ -111,9 +51,12 @@ function attachScript(tab) {
         }
       });
     }  
-    notifications.notify({
-      text: "You were logged out",
-    });
+    if(notificationPref == 1)
+    {
+      notifications.notify({
+        text: "You are now idle.",
+      });
+    }
     worker.destroy();
   })
 
@@ -121,6 +64,11 @@ function attachScript(tab) {
   worker.port.emit("getIdleUnit", unit);
 }
 
-/*function destroyScript(worker) {
+function changePreference(prefName) {
+  console.log(prefName + " has changed value");
 
-}*/
+  time = preferences.idleTime;
+  unit = preferences.idleUnit;
+  list = preferences.urlList;
+  notificationPref = preferences.notification;
+}
